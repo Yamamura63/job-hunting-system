1cc421051846430c8c3e2fdc8320d8a133e78337
Yamamura63 <Yamamura.maiko630@gmail.com>
2026-07-08 13:55:46 +0900

    登録ページの完成

--- src/resources/views/companies/create.blade.php
+++ src/resources/views/companies/create.blade.php
@@ -8,38 +8,38 @@
     <div class="mx-auto max-w-6xl rounded-lg bg-white p-8 shadow flex justify-center">
         <form method="POST" action="{{ route('company.store') }}" class="w-full max-w-2xl">
             @csrf
-            <div class="m-1">
-                <label class="text-lg font-bold">企業名</label><br>
+            <div class="m-3">
+                <label class="text-lg font-bold">企業名</label>
                 <input type="text" name="name" required class="w-full m-2 border border-gray-500 rounded">
             </div>
 
-            <div clas="m-1">
-                <label class="text-lg font-bold">志望度</label><br>
+            <div class="m-3">
+                <label class="text-lg font-bold">志望度</label>
                 <div class="rate-form">
-                    <input id="star5" type="radio"  name="level" value="5" class="hidden">
+                    <input id="star5" type="radio" name="level" value="5" class="hidden">
                     <label for="star5">★</label>
-                    <input id="star4" type="radio"  name="level" value="4" class="hidden">
+                    <input id="star4" type="radio" name="level" value="4" class="hidden">
                     <label for="star4">★</label>
-                    <input id="star3" type="radio"  name="level" value="3" class="hidden" {{ old('rate', 3) == 3 ? 'checked' : '' }}>
+                    <input id="star3" type="radio" name="level" value="3" class="hidden" {{ old('rate', 3) == 3 ? 'checked' : '' }}>
                     <label for="star3">★</label>
-                    <input id="star2" type="radio"  name="level" value="2" class="hidden">
+                    <input id="star2" type="radio" name="level" value="2" class="hidden">
                     <label for="star2">★</label>
-                    <input id="star1" type="radio"  name="level" value="1" class="hidden">
+                    <input id="star1" type="radio" name="level" value="1" class="hidden">
                     <label for="star1">★</label>
                 </div>
             </div>
 
-            <div clas="m-1">
+            <div class="m-3">
                 <label class="text-lg font-bold">本社所在地</label><br>
                 <input type="text" name="address" class="w-full m-2 border border-gray-500 rounded">
             </div>
 
-            <div clas="m-1">
+            <div class="m-3">
                 <label class="text-lg font-bold">業種</label><br>
                 <input type="text" name="industry" class="w-full m-2 border border-gray-500 rounded">
             </div>
 
-            <div clas="m-1">
+            <div class="m-3">
                 <label class="text-lg font-bold">給与</label>
                 <div class="flex items-center">
                     <p class="text-lg">合計：</p>
@@ -57,7 +57,7 @@
                 </div>
             </div>
 
-            <div clas="m-1">
+            <div class="m-3">
                 <label class="text-lg font-bold">勤務時間</label><br>
                 <div class="ml-4">
                     <div class="flex items-center">
@@ -76,12 +76,12 @@
                 </div>
             </div>
 
-            <div clas="m-1">
+            <div class="m-3">
                 <label class="text-lg font-bold">SES度</label><br>
                 <div class="flex items-center gap-2">
                     <input id="no" type="radio" name="ses_level" value="5">
                     <label for="no">なし</label>
-                    <input id="low" type="radio" name="ses_level" value="4">
+                    <input id="low" type="radio" name="ses_level" value="4" checked>
                     <label for="low">低</label>
                     <input id="high" type="radio" name="ses_level" value="3">
                     <label for="high">高</label>
@@ -90,18 +90,16 @@
                 </div>
             </div>
 
-            <div clas="m-1">
+            <div class="m-3">
                 <label class="text-lg font-bold">福利厚生メモ</label>
-                <p class="error text-red-500"></p>
                 <textarea name="benefits_memo" rows="6"
-                    class="body target_length resize-none overflow-y-auto m-2 border rounded w-full"></textarea>
+                    class="w-full resize-none m-2 border rounded"></textarea>
             </div>
 
-            <div clas="m-1">
+            <div class="m-3">
                 <label class="text-lg font-bold">メモ</label>
-                <p class="error text-red-500"></p>
                 <textarea name="free_memo" rows="6"
-                    class="body target_length resize-none overflow-y-auto m-2 border rounded w-full"></textarea>
+                    class="w-full resize-none m-2 border rounded"></textarea>
             </div>
 
             <button type="submit" class="cursor-pointer text-lg text-white bg-blue-400 rounded pt-2 pb-2 pl-4 pr-4">✐登録</button>
@@ -130,28 +128,6 @@
 
     kihon.addEventListener('input', calcSalary);
     other.addEventListener('input', calcSalary);
-
-
-    const text = document.querySelector('.body');
-    const count = document.getElementById('count');
-    const target = document.querySelector('.target_length');
-    const error = document.querySelector('.error');
-
-    function checkLength() {
-        const length = text.value.length;
-
-        count.textContent = length;
-
-        if (length > target.value) {
-            error.textContent =
-                `入力は${length}文字までです。`;
-        } else {
-            error.textContent = '';
-        }
-    }
-
-    text.addEventListener('input', checkLength);
-    target.addEventListener('change', checkLength);
 </script>
 
 @endsection
