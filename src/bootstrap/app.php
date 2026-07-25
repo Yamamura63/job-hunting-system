800a54e94be49dade462155f3c757362c8ea3092
Yamamura63 <Yamamura.maiko630@gmail.com>
2026-07-25 23:10:17 +0900

    Trust Render proxy for HTTPS

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -11,15 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-    $middleware->appendToGroup('web', [
-        \Illuminate\Cookie\Middleware\EncryptCookies::class,
-        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
-        \Illuminate\Routing\Middleware\SubstituteBindings::class,
-    ]);
-})
+        $middleware->trustProxies(at: '*');
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
