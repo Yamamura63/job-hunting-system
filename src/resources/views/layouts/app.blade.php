dddfbb0237e1f8d1be522dd12ac2deb96e09c937
Yamamura63 <Yamamura.maiko630@gmail.com>
2026-07-25 12:06:15 +0900

    ハンバーガーメニュー設定

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
         <!-- Tailwind CSS CDN
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> -->
-        @vite('resources/css/app.css')
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
