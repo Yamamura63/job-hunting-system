c2647e42e72d8c219d426e4832a55083547677c0
Yamamura63 <Yamamura.maiko630@gmail.com>
2026-07-27 11:39:34 +0900

    更新

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -1,28 +1,22 @@
 const CACHE_NAME = 'job-hunting-system-v1';
 
-const CACHE_URLS = [
-    '/',
-    '/login',
-    '/build/assets/app-BFRdXGZ1.css',
-    '/build/assets/app-BBPB1kKK.js'
-];
-
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(CACHE_URLS))
-    );
-
     self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.add('/login');
+        })
+    );
 });
 
 self.addEventListener('activate', event => {
     event.waitUntil(
-        caches.keys().then(keys => {
+        caches.keys().then(cacheNames => {
             return Promise.all(
-                keys
-                    .filter(key => key !== CACHE_NAME)
-                    .map(key => caches.delete(key))
+                cacheNames
+                    .filter(cacheName => cacheName !== CACHE_NAME)
+                    .map(cacheName => caches.delete(cacheName))
             );
         })
     );
